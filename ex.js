a382bb063aab3cf09acae991c2024ff4ca937d89
Aly Mohamed <alyabdulmottalib@gmail.com>
2022-03-16 12:58:01 +0200

    task

--- ex.js
+++ ex.js
@@ -1,34 +1,65 @@
-
 function printRecords(recordIds) {
-	// TODO
-
+  let thing1 = recordIds
+    .map((id) => studentRecords.find((stud) => stud.id == id))
+    .sort((el1, el2) => {
+      if (el1.name > el2.name) {
+        return 1;
+      } else if (el1.name < el2.name) {
+        return -1;
+      } else return 0;
+    });
+  thing1?.forEach((element) => {
+    console.log(
+      `${element.name}(${element.id}): ${
+        (element.paid && "paid") || (!element.paid && "not paid")
+      }`
+    );
+  });
 }
 
 function paidStudentsToEnroll() {
-	// TODO
-
+  let paidAndEnrolled = studentRecords.filter(
+    (element) => currentEnrollment.includes(element.id) || element.paid
+  );
+  return paidAndEnrolled.map((x) => x.id);
 }
 
 function remindUnpaid(recordIds) {
-	// TODO
+  let unpaidStuds = studentRecords.filter(
+    (x) => recordIds.includes(x.id) && x.paid !== true
+  );
 
+  unpaidStuds
+    .sort((el1, el2) => {
+      if (el1.name > el2.name) {
+        return 1;
+      } else if (el1.name < el2.name) {
+        return -1;
+      } else return 0;
+    })
+    .forEach((element) => {
+      console.log(
+        `${element.name}(${element.id}): ${
+          (element.paid && "paid") || (!element.paid && "not paid")
+        }`
+      );
+    });
 }
-
 
 // ********************************
 
-var currentEnrollment = [ 410, 105, 664, 375 ];
+var currentEnrollment = [410, 105, 664, 375];
 
 var studentRecords = [
-	{ id: 313, name: "Frank", paid: true, },
-	{ id: 410, name: "Suzy", paid: true, },
-	{ id: 709, name: "Brian", paid: false, },
-	{ id: 105, name: "Henry", paid: false, },
-	{ id: 502, name: "Mary", paid: true, },
-	{ id: 664, name: "Bob", paid: false, },
-	{ id: 250, name: "Peter", paid: true, },
-	{ id: 375, name: "Sarah", paid: true, },
-	{ id: 867, name: "Greg", paid: false, },
+  { id: 313, name: "Frank", paid: true },
+  { id: 410, name: "Suzy", paid: true },
+  { id: 709, name: "Brian", paid: false },
+  { id: 105, name: "Henry", paid: false },
+  { id: 502, name: "Mary", paid: true },
+  { id: 664, name: "Bob", paid: false },
+  { id: 250, name: "Peter", paid: true },
+  { id: 375, name: "Sarah", paid: true },
+  { id: 867, name: "Greg", paid: false },
 ];
 
 printRecords(currentEnrollment);
